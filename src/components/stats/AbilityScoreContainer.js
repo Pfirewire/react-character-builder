f2f0e15d4917d8b464ab3c835c7943ea71a9fb29
Stephen Pflug <stephen@pflug.us>
2023-03-28 19:30:10 -0500

    Added Container styled component to AbilityScoreContainer component

--- src/components/stats/AbilityScoreContainer.js
+++ src/components/stats/AbilityScoreContainer.js
@@ -1,15 +1,16 @@
 import AbilityScore from "./AbilityScore";
+import {Container} from "../styles/Container.styled";
 
 function AbilityScoreContainer() {
     return(
-        <div>
+        <Container>
             <AbilityScore type={'STR'} />
             <AbilityScore type={'DEX'} />
             <AbilityScore type={'CON'} />
             <AbilityScore type={'WIS'} />
             <AbilityScore type={'INT'} />
             <AbilityScore type={'CHA'} />
-        </div>
+        </Container>
     );
 }
 
