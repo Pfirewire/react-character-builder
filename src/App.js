c68ca34f60bcdfd4b86ba7fdd164568896982e35
Stephen Pflug <stephen@pflug.us>
2023-03-28 19:22:01 -0500

    Added AbilityScoreContainer to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import AbilityScoreContainer from "./components/stats/AbilityScoreContainer";
 
 
 function App() {
     return(
         <div>
             Character Builder App Incoming!
+            <br/><br/><br/><br/>
+            <AbilityScoreContainer />
         </div>
     );
 }
