c3934fa454146fafd87d4a7a8f79f19d77fb77f2
Stephen Pflug <stephen@pflug.us>
2023-04-07 09:41:00 -0500

    Added styled component for larger input element

---
+++ src/components/styles/BiggerInput.styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const BiggerInput = styled.input`
+  line-height: 1.5em;
+`;
--- src/components/styles/BodyColumn.styled.js
+++ src/components/styles/BodyColumn.styled.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const BodyColumn = styled.div`
   width: 320px;
   display: flex;
