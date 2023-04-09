d60339b609dd3df07df24ddca22eb125aafbdfa4
Stephen Pflug <stephen@pflug.us>
2023-04-08 20:05:06 -0500

    Added functionality to edit xp

--- src/components/header/quick-info/XP.js
+++ src/components/header/quick-info/XP.js
@@ -1,12 +1,28 @@
 import styled from "styled-components";
 import {useDispatch, useSelector} from "react-redux";
+import {setXP, setXPMax} from "../../../store/slices/characterSlice";
+import {BiggerInput} from "../../styles/BiggerInput.styled";
 
 function XP() {
     const dispatch = useDispatch();
     const {xp} = useSelector(state => state.character);
+
+    const handleXPChange = e => {
+        dispatch(setXP(e.target.value));
+    };
+
+    const handleMaxXPChange = e => {
+        dispatch(setXPMax(e.target.value));
+    };
+
     return(
         <XPWrapper>
-            XP
+            <XPFieldWrapper>
+                <BiggerInput value={xp[0]} onChange={handleXPChange} />
+                <div>/</div>
+                <BiggerInput value={xp[1]} onChange={handleMaxXPChange} />
+            </XPFieldWrapper>
+            <label>XP (current / max)</label>
         </XPWrapper>
     );
 }
@@ -14,8 +30,13 @@ function XP() {
 export default XP;
 
 const XPWrapper = styled.div`
-  width: 100%;
+  width: 150px;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const XPFieldWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
