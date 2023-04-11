4a6ce3a448a00f3538790bcbbc1fb52022e94c60
Steve Pflug <stephen@pflug.us>
2023-04-11 09:16:27 -0500

    Added logic to include proficiency bonus input and have it change character state

--- src/components/body/stats/ProficiencyBonus.js
+++ src/components/body/stats/ProficiencyBonus.js
@@ -1,9 +1,22 @@
 import styled from "styled-components";
+import {useDispatch, useSelector} from "react-redux";
+import {setProficiencyBonus} from "../../../store/slices/characterSlice";
 
 function ProficiencyBonus() {
+    const dispatch = useDispatch();
+    const {proficiencyBonus} = useSelector(state => state.character);
+
+    const handleProficiencyBonusChange = e => {
+        dispatch(setProficiencyBonus(e.target.value));
+    };
+
     return(
         <ProficiencyBonusWrapper>
-            ProficiencyBonus
+            <div>
+                <span>+</span>
+                <ProficiencyBonusInput type={'number'} min={'0'} max={'6'} value={proficiencyBonus} onChange={handleProficiencyBonusChange} />
+            </div>
+            <label>ProficiencyBonus</label>
         </ProficiencyBonusWrapper>
     );
 }
@@ -12,4 +25,11 @@ export default ProficiencyBonus;
 
 const ProficiencyBonusWrapper = styled.div`
   height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProficiencyBonusInput = styled.input`
+  width: 36px;
 `;
