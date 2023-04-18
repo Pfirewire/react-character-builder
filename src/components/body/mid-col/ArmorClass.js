e23533a1ec82f26c1092bf25be31e131fc296bbc
Steve Pflug <stephen@pflug.us>
2023-04-18 10:56:54 -0500

    displaying armor class and some styling

--- src/components/body/mid-col/ArmorClass.js
+++ src/components/body/mid-col/ArmorClass.js
@@ -1,9 +1,17 @@
 import styled from "styled-components";
+import {useSelector} from "react-redux";
 
 function ArmorClass() {
+    const {armorClass} = useSelector(state => state.character);
+
     return(
         <ArmorClassWrapper>
-            ArmorClass
+            <ArmorClassValueWrapper>
+                {armorClass}
+            </ArmorClassValueWrapper>
+            <ArmorClassTextWrapper>
+                Armor Class
+            </ArmorClassTextWrapper>
         </ArmorClassWrapper>
     );
 }
@@ -12,4 +20,18 @@ export default ArmorClass;
 
 const ArmorClassWrapper = styled.div`
   flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ArmorClassValueWrapper = styled.div`
+  flex-grow: 1;
+  font-size: 3em;
+  display: flex;
+  align-items: center;
+`;
+
+const ArmorClassTextWrapper = styled.p`
+  
 `;
